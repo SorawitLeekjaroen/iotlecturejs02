2df7a6d25567f964b602f9ef7d4f7b0c6d12cad5
SorawitLeekjaroen <springtrap2009@gmail.com>
2025-03-22 20:12:41 +0700

    ทำ 10-17 เสร็จแล้ว

--- study13.js
+++ study13.js
@@ -2,10 +2,23 @@
 // คือ การทำงานหนึ่งๆ จะไม่ทำงานหากเราไม่เรียกใช้ (call function)
 //------------------------------
 //3. no parameter, has return
-
+function show3(){
+    console.log('Wow...');
+    return 10
+}
 
 
 //4. have parameter, has return
+function show4(x,y){
+    console.log('Hey...');
+    return x**y
+}
+    console.log(show3());
+    
+    let data1 = show3();
+    let data2 = show4(2,3);
 
+    console.log(data1 + data2);
+    
 
 
